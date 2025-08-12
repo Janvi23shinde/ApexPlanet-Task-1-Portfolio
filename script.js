efe8e68bf521e036f38612c6f0b3704458b75781
Janvi23shinde <janvishinde866@gmail.com>
2025-08-12 22:43:24 +0530

    Add files via upload
    Initial commit for Task-1

---
+++ script.js
@@ -0,0 +1,11 @@
+function showAlert() {
+  alert("Thank you for choosing Janvi's Closet! Happy Shopping!");
+}
+
+function loginAlert() {
+  alert("Login feature coming soon!");
+}
+
+function registerAlert() {
+  alert("Register feature coming soon!");
+}
